887559bcc1d169e2ea7b370a2f0e1a37190318a7
gtn-74 <gtn.r74@gmail.com>
2024-07-09 13:39:50 +0900

    [add]create todo

--- src/components/create-todo-form.tsx
+++ src/components/create-todo-form.tsx
@@ -1,21 +1,25 @@
+import { addTodo } from "@/lib/action";
+
 export const CreateTodoForm = () => {
-    return (
-      <form>
-        <div className="flex items-center justify-center space-x-3">
-          <input
-            required
-            placeholder="TODO を入力"
-            type="text"
-            className="max-w-sm bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-blue-500 block w-full p-2 cursor-not-allowed"
-            defaultValue=""
-          />
-          <button
-            type="submit"
-            className="px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg cursor-pointer hover:bg-blue-800 focus:ring-4 focus:ring-blue-300"
-          >
-            追加
-          </button>
-        </div>
-      </form>
-    );
-  };
+  return (
+    <form action={addTodo}>
+      <div className="flex items-center justify-center space-x-3">
+        <input
+          required
+          placeholder="TODO を入力"
+          // inputとname紐づけてないのにどうやってリンクさせてんねんって話
+          name="title"
+          type="text"
+          className="max-w-sm bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-blue-500 block w-full p-2 cursor-not-allowed"
+          defaultValue=""
+        />
+        <button
+          type="submit"
+          className="px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg cursor-pointer hover:bg-blue-800 focus:ring-4 focus:ring-blue-300"
+        >
+          追加
+        </button>
+      </div>
+    </form>
+  );
+};
